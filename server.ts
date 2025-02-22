17435b9fef9d7a5cf40fc8b7cb7befc44c6e9af6
techtony92 <techtony92@gmail.com>
2025-02-22 03:56:32 -0500

    initial setup of app testing suite

--- server.ts
+++ server.ts
@@ -1,23 +1,8 @@
-import express, {Request, Response} from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app = express();
+import server from "./serverConfiguration";
 const PORT = process.env.PORT || 4500;
 
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
-app.get("/", (req:Request, res:Response ) =>{
-    console.log(` @${req.ip} \n Date: ${new Date().getMonth()}/${new Date().getDate()}/${new Date().getFullYear()} Time: @${new Date().getHours()}-${new Date().getMinutes()}${new Date().getHours() < 12 ? `am`:`pm`}`);
-        res.send({msg:"Request Working"});
-})
-
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`Server Listening on PORT ${PORT}`);
 })
-
-
-export default app;
---
+++ serverConfiguration.ts
@@ -0,0 +1,17 @@
+import express, {type Request, type Response, type Express} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server:Express = express();
+
+
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
+server.get("/", (req:Request, res:Response ) =>{
+    console.log(` @${req.ip} \n Date: ${new Date().getMonth()}/${new Date().getDate()}/${new Date().getFullYear()} Time: @${new Date().getHours()}-${new Date().getMinutes()}${new Date().getHours() < 12 ? `am`:`pm`}`);
+        res.send({msg:"Request Working"});
+})
+
+export default server;
